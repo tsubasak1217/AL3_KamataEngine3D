7356a125d7b2efea117509649b406d5290518980
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-04-10 09:33:02 +0900

    setting

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2A_12_クロカワ_ツバサ");
+	win->CreateGameWindow(L"LE2A_12_クロカワ_ツバサshoki");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
